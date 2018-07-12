64e0994ff853ecc06cfd9ef8087fff18f6dd7fe1
tusharkh <tushark@uw.edu>
2018-07-11 17:11:28 -0800

    got ensembles to work

--- latin_hypercube.py
+++ latin_hypercube.py
@@ -2,6 +2,7 @@ import pyDOE as pe
 import numpy as np
 import copy
 import pandas as pd
+import v2_run_calibration_4Tushar as v2
 
 def sample1(tempchange, precfactor, ddfsnow,
             samples=10, criterion=None):
@@ -67,55 +68,45 @@ def sample1(tempchange, precfactor, ddfsnow,
     # copy the given traces
     tempchange_copy = copy.deepcopy(tempchange)
     precfactor_copy = copy.deepcopy(precfactor)
-    ddfsnow_snow = copy.deepcopy(ddfsnow)
+    ddfsnow_copy = copy.deepcopy(ddfsnow)
 
     # debug
-    print('copy of tempchange:', tempchange_copy)
-    print('copy of precfactor:', precfactor_copy)
-    print('copy of ddfsnow:', ddfsnow_copy)
+#    print('copy of tempchange:', tempchange_copy)
+#    print('copy of precfactor:', precfactor_copy)
+#    print('copy of ddfsnow:', ddfsnow_copy)
 
     traces = [tempchange_copy, precfactor_copy,
               ddfsnow_copy]
 
     # sort the traces
     for trace in traces:
-        traces.sort()
+        trace.sort()
 
-#    tempchange_copy.sort()
-#    precfactor_copy.sort()
-#    ddfsnow_copy.sort()
 
     # debug
-    print('sorted copy of tempchange:', tempchange_copy)
-    print('sorted copy of precfactor:', precfactor_copy)
-    print('sorted copy of ddfsnow:', ddfsnow_copy)
+#    print('sorted copy of tempchange:', tempchange_copy)
+#    print('sorted copy of precfactor:', precfactor_copy)
+#    print('sorted copy of ddfsnow:', ddfsnow_copy)
 
     lhd = pe.lhs(n=3, samples=samples, criterion=criterion)
 
-#    for dist in dists_copy:
-#        dist.sort()
-#    print('copy of sorted argument:', dists_copy)
-#    lhd = pe.lhs(n=len(dists_copy), samples=samples,
-#                 criterion=criterion)
 
     # debug
-    print('copy of lhs samples:', lhd)
+#    print('copy of lhs samples:', lhd)
 
     lhd = np.ndarray.transpose(lhd)
 
     # debug
-    print('copy of lhs samples transposed:', lhd)
+#    print('copy of lhs samples transposed:', lhd)
 
     for i in range(len(traces)):
         lhd[i] *= len(traces[i])
 
-#    for i in range(0, len(dists_copy)):
-#        lhd[i] = len(dists_copy[i]) * lhd[i]
 
     lhd = lhd.astype(int)
 
     # debug
-    print('copy of lhs samples ints:', lhd)
+#    print('copy of lhs samples ints:', lhd)
 
     names = ['tempchange', 'precfactor', 'ddfsnow']
     samples = pd.DataFrame()
@@ -123,24 +114,77 @@ def sample1(tempchange, precfactor, ddfsnow,
     for i in range(len(traces)):
         array = []
         for j in range(len(lhd[i])):
-            print(i, j, traces[i][lhd[i][j]])
+#            print(i, j, traces[i][lhd[i][j]])
             array.append(traces[i][lhd[i][j]])
         samples[names[i]] = array
 
-#    matrix = []
-#    for i in range(0, len(lhd)):
-#        array = []
-#        for j in range(0, len(lhd[i])):
-#            print(i, j,  dists_copy[i][lhd[i][j]])
-#            array.append(dists_copy[i][lhd[i][j]])
-#        matrix.append(np.array(array))
-
-#    result = np.array(matrix)
-#    result = np.ndarray.transpose(result)
-
-    print(samples, type(samples))
+#    print(samples, type(samples))
     return samples
 
+def find_mass_balace(samples, replace=True):
+    """
+    Calculated mass balances for given set of
+    parameters
+
+    Takes a pandas dataframe of sample sets and
+    adds a column for the mass balances (average
+    annual change in S.W.E. over the
+    time period of David Shean's observations)
+    Takes a list of random probability distributions
+    (each in the form of arrays of numbers) and
+    and performs a latin hypercube sampling of these
+    dataframes. Returns a new dataframe (can replace
+    the old one if desired) with the mass balances
+    included.
+
+    Parameters
+    ----------
+    samples : pandas.dataframe
+        A dataframe where each row represents sets of
+        parameters for the model.Columns are 'tempchange',
+        'precfactor' and 'ddfsnow'
+    replace : boolean
+        True if dataframe should be unchanged and a new
+        dataframe should be returned, if False the given
+        dataframe will be modified and a copy will not be
+        created. default True
+
+    Returns
+    -------
+    pandas.dataframe
+         A dataframe where each row represents sets of
+        parameters for the model and the mass balance
+        that results from using these parameters.
+        Columns are 'tempchange','precfactor', 'ddfsnow'
+        and 'massbalance'
+
+    """
+
+    # make copy if replace is True
+    if replace:
+        samples = copy.deepcopy(samples)
+
+    # calculate massbalances row by row
+    massbalances = []
+    for index, row in samples.iterrows():
+        # debug
+#        print(row['precfactor'], row['tempchange'], row['ddfsnow'])
+
+        # caluclate mass balance for each set of params
+        massbalance = v2.get_mass_balance(precfactor=row['precfactor'],
+                                          tempchange=row['tempchange'],
+                                          ddfsnow=row['ddfsnow'])
+
+        # debug
+#        print('massbalance', massbalance)
+
+        # add to dataframe
+        massbalances.append(massbalance)
+
+    # add to dataframe
+    samples['massbalance'] = massbalances
+
+    return samples
 
 def sample(distributions, samples=10, criterion=None):
     """
