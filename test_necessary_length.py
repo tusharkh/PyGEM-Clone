8f326986b7dd6d253ae464cdad70c7c46239c7aa
tusharkh <tushark@uw.edu>
2018-07-29 08:16:06 -0800

    further convergence testing

--- test_necessary_length.py
+++ test_necessary_length.py
@@ -2,7 +2,7 @@ import pymc_model as pm
 import pymc
 import numpy as np
 
-for i in [20000, 30000]:
+for i in [30000]:
     print('completing run ' + str(i))
-    name = 'testlength' + str(i) + '.pickle'
+    name = 'test2length' + str(i) + '.pickle'
     m = pm.run_MCMC(iterations=i, dbname=name)
